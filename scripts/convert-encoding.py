af41775489b8d247e77e5638e1ceec0444220533
Stan van Rooy <me@stanvanrooy.com>
2024-02-26 13:58:32 +0100

    f

--- scripts/convert-encoding.py
+++ scripts/convert-encoding.py
@@ -5,28 +5,20 @@ old_translations = sys.argv[1]
 new_translations = sys.argv[2]
 output_file = sys.argv[3]
 
-old_translations_f = open(old_translations, 'r')
-new_translations_f = open(new_translations, 'r', encoding='ascii', errors='ignore')
+old_translations_f = open(old_translations, 'r', encoding='utf-8')
+new_translations_f = open(new_translations, 'r', encoding='windows-1252', errors='ignore')
 output_file_f = open(output_file, 'w', encoding='utf-8')
 
 english_new_lang_mapping = {}
 
-with new_translations_f as new_file:
-    reader = csv.reader(new_file, delimiter=';')
-
-    for row in reader:
-        english_value = row[0]
-        new_value = row[-1].encode('utf-8').decode('utf-8')
-        english_new_lang_mapping[english_value] = new_value
-
+new_translations = [l.split(';')[-1] for l in new_translations_f.readlines()]
 
 with old_translations_f as old_file, output_file_f as output_file:
     reader = csv.reader(old_file, delimiter=';')
     writer = csv.writer(output_file, delimiter=';')
 
-    for row in reader:
-        english_value = row[0]
-        new_value = english_new_lang_mapping.get(english_value, '')
+    for i, row in enumerate(reader):
+        new_value = new_translations[i]
         new_row = row + [new_value.strip()]
         writer.writerow(new_row)
 
