f8dc551dbcd39e190057d8f2075a5113ab0a31a8
Stan van Rooy <me@stanvanrooy.com>
2024-04-22 13:44:11 +0200

    fix

--- scripts/test-duplicate-key.py
+++ scripts/test-duplicate-key.py
@@ -7,7 +7,12 @@ with open(translations, 'r') as f:
 
 keys = set()
 for line_no, line in enumerate(lines, start=1):
-    key = line.split(',')[0]
+    key = line.split(';')[0]
     if key in keys:
         print(f'Duplicate key found in line {line_no}: {key}')
     keys.add(key)
+
+if len(keys) == len(lines):
+    exit(0)
+exit(1)
+
